28486ed896e609f1fc814cefc7338a9009bbb189
halfwaypercent <160871440+halfwaypercent@users.noreply.github.com>
2025-05-23 12:11:41 +0000

    oi

---
+++ aula9/ex1.py
@@ -0,0 +1,7 @@
+from time import sleep
+contador = 1
+   
+while contador <=10:
+        print(contador*2)
+        sleep(1)
+        contador += 1
--- ex1.py
+++ ex1.py
@@ -1,7 +1,9 @@
-from time import sleep
-contador = 1
-   
-while contador <=10:
-        print(contador*2)
-        sleep(1)
+frase = input("Digite uma frase: ")
+palavra = input("Digite a palavra a ser buscada: ")
+
+contador = 0
+for i in range(len(frase) - len(palavra) + 1):
+    if frase[i:i+len(palavra)] == palavra:
         contador += 1
+
+print(f'A palavra "{palavra}" aparece {contador} vez(es) na frase.')
